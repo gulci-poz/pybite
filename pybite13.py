95cc32eeba074b4e05b2ef3bed98fdebbd9ae315
gulci <sebastian.gulczynski.poznan@gmail.com>
2015-10-27 10:39:04 +0100

    2015-10-27 time for modules and classes

--- pybite13.py
+++ pybite13.py
@@ -2,9 +2,25 @@
 
 def wczytaj_od_usera():
     """
-    Pobiera od użytkownika napis i zapisuje do pliku
+    Pobiera od użytkownika napis i zapisuje do pliku, działa do pojawienia się pustej linii
     """
-    pass
+    napis = input("Podaj napis do zapamiętania > ")
+
+    with open("imiona", "a") as plik:
+        while napis:
+            plik.write(napis + "\n")
+            napis = input("Podaj napis do zapamiętania > ")
 
 def wczytaj_z_pliku():
-    pass
+    with open("imiona", "r") as plik:
+        linijki = plik.readlines()
+        # result jest dostępny w całej funkcji, nie tylko w with
+        result = {idx + 1: linia.strip() for idx, linia in enumerate(linijki)}
+    return result
+
+#wczytaj_od_usera()
+imiona_dict = wczytaj_z_pliku()
+print(imiona_dict)
+
+# SPHINX - tworzenie dokumentacji
+# opiera się na reStructuredText, jest to w pewnym stopniu pochodna markdown
