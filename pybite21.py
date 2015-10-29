0bbbac0350e4dd0af82982d3f700f8d16e7b539b
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-10-30 00:09:17 +0100

    2015-10-30 decorators intro

--- pybite21.py
+++ pybite21.py
@@ -30,11 +30,18 @@ except MyOwnException:
     print("Błąd MyOwnException")
 except IndexError:
     print("Brak indeksu")
-    # można tutaj wyrzucić wyjątek (pójdzie wyżej), pojawi się po wykonaniu try
-    raise
+    # obsługujemy wyjątek na ile to możliwe
+    # można potem wyrzucić wyjątek (pójdzie wyżej), pojawi się po wykonaniu try
+    # dobry sposób na zakończenie programu, gdy doszliśmy do oczekiwanego wyniku, jesteśmy wewnątrz zagnieżdżonej struktury i nie potrzebujemy kończyć jej wykonywania (zamiast bawić się w break), np. raise "ItemFound" - mamy oczekiwany wynik, obsługujemy wyjątek, wyrzucamy go i wyskakujemy z programu (błąd: operacja zakończona powodzeniem)
+    # jeśli wyjątek nie będzie obsłużony, to będzie się piął coraz wyżej w hierarchi programu aż w końcu zostanie zgłoszony do procesu głównego i program w końcu się wywali
+
+    # dajemy raise gdzieś w try, po zakończeniu obliczeń, tutaj wywalimy program
+    #raise
+
 # else się wykonuje, jeśli pozostałe wyjątki nie wystąpiły
 else:
     print("Jestem w else.")
+
 # finally zawsze się wykonuje
 # tutaj sprzątamy, np. usuwamy obiekty
 finally:
@@ -42,4 +49,23 @@ finally:
 
 print("Po wszystkim.")
 
-# 7 - 1 g 6 min
+##################################################
+
+class ItemFound(Exception):
+    pass
+
+try:
+    for i in range(0, 100):
+        for j in range(0, 300):
+            for k in range(3, 500):
+                if k == j ** i:
+                    raise ItemFound()
+                    # wyrzucamy wyjątek, który potem jest obsłużony
+
+except ItemFound as e:
+    # mamy dostęp do obiektu wyjątku za pomocą e, możemy to zapisać do logu
+    # --> logging facility for python
+
+    print("found i j k:", i, j, k)
+
+    # tutaj nie dajemy raise, chyba że chcemy wywalić program
---
+++ pybite22.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+print("type(1):", type(1))
+
+# w etykieta opakowujemy typ integer
+etykieta = type(1)
+print("type(etykieta):", type(etykieta))
+print("etykieta:", etykieta)
+
+print("int(3.14):", int(3.14))
+print("etykieta(3.14):", etykieta(3.14))
+
+
+def f(x, y):
+    return x + y
+
+print("type(f):", type(f))
+
+# mamy nową etykietę dla funkcji f
+g = f
+
+print("g(1, 2):", g(1, 2))
+print("g:", g)
+print("type(g):", type(g))
+
+
+def m(x, y):
+    return x - y
+
+def operacja(operator, x, y):
+    return operator(x, y)
+
+print("operacja f:", operacja(f, 1, 2))
+print("operacja m:", operacja(m, 1, 2))
+
+# dekorator opakowuje funkcję w dodatkowe czary-mary ;)
+
+def operacja2(operator, x, y):
+    return operator(x, y) + 5
+
+def dek(f, *args, **kwargs):
+    return f(*args, **kwargs)
+
+# stara notacja dekoratora
+# nazwa dekoratora
+@dekorator
+def funkcja(x, y):
+    return x + y
+
+# opakowanie funkcji w dekorator
+#funkcja = dekorator(funkcja)
+
+# 7 ~ 1 g 29 min
